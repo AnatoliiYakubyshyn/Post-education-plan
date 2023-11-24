86c50a2c532430ca229c70ab2a967cd542efda20
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2023-11-24 13:48:41 +0200

    implemented data-driven#2

--- src/test/java/com/solvd/SignUpTest.java
+++ src/test/java/com/solvd/SignUpTest.java
@@ -1,5 +1,7 @@
 package com.solvd;
 
+import com.solvd.gui.components.SignUpForm;
+import com.solvd.utility.ReadDataFromCSV;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -23,6 +25,27 @@ public class SignUpTest extends AbstractTest {
         Assert.assertTrue(accountPage.isPageOpened() || signUpPage.isErrorMessageDisplayed(),
                 "Test failed with user "+user.toString());
     }
+
+
+    private String weakPassword = "Password Strength: Weak";
+
+    @Test(dataProvider = "weakPassProvider", testName = "data-driven#2")
+    public void testWeakPassword(String password) {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened());
+        SignUpPage signUpPage = homePage.getHeaderMenu().clickSignUpPage();
+        SignUpForm signUpForm = signUpPage.getSignUpForm();
+        signUpForm.fillPasswordField(password);
+        Assert.assertEquals(signUpForm.getPasswordStrength().trim(),weakPassword,
+                signUpForm.getPasswordStrength().trim());
+    }
+
+    @DataProvider(name = "weakPassProvider",parallel = true)
+    public Object[][] weakPassProvider() {
+        return ReadDataFromCSV.readCsv("src/test/resources/data/weak_passwords.csv");
+    }
+
 
     @DataProvider(name = "userProvider",parallel = true)
     public Object[][] userProvider() {
