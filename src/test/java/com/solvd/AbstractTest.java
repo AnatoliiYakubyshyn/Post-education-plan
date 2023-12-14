39e31a3860b2aaed8353a959d9f9b94462394460
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2023-12-14 12:36:48 +0200

    implemented screenshoting

--- src/test/java/com/solvd/AbstractTest.java
+++ src/test/java/com/solvd/AbstractTest.java
@@ -1,13 +1,20 @@
 package com.solvd;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.time.Duration;
 
@@ -46,7 +53,11 @@ public abstract class AbstractTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult testResult, Method method) throws IOException {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            File scrFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile,new File("src/test/resources/screenshots/"+method.getDeclaringClass()+" "+method.getName()+".jpg"));
+        }
         getDriver().quit();
     }
 }
